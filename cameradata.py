e957880fb58ebaffa92007bf69f16d47ca37e6e9
Sourcery AI <>
2023-11-23 13:22:19 +0000

    'Refactored by Sourcery'

--- cameradata.py
+++ cameradata.py
@@ -15,7 +15,7 @@ def get_3x4_RT_matrix_from_blender(cam):
          (0, -1, 0),
          (0, 0, -1)))
 
-    location, rotation = cam.matrix_world.decompose()[0:2]
+    location, rotation = cam.matrix_world.decompose()[:2]
     R_world2bcam = rotation.to_matrix().transposed()
 
     T_world2bcam = -1*R_world2bcam @ location
@@ -23,13 +23,13 @@ def get_3x4_RT_matrix_from_blender(cam):
     R_world2cv = R_bcam2cv@R_world2bcam
     T_world2cv = R_bcam2cv@T_world2bcam
 
-    # put into 3x4 matrix
-    RT = Matrix((
-        R_world2cv[0][:] + (T_world2cv[0],),
-        R_world2cv[1][:] + (T_world2cv[1],),
-        R_world2cv[2][:] + (T_world2cv[2],)
-         ))
-    return RT
+    return Matrix(
+        (
+            R_world2cv[0][:] + (T_world2cv[0],),
+            R_world2cv[1][:] + (T_world2cv[1],),
+            R_world2cv[2][:] + (T_world2cv[2],),
+        )
+    )
 
 
 # TODO: select main camera here
@@ -37,7 +37,7 @@ cam = bpy.data.objects["Camera_L"]
 
 worldmat = get_3x4_RT_matrix_from_blender(cam)
 worldmat = np.vstack((worldmat, np.asarray([[0,0,0,1]])))
-np.savetxt(f"extrinsics.txt", worldmat)
+np.savetxt("extrinsics.txt", worldmat)
 
 intrinsics = get_intrinsics(cam.data.lens)
-np.savetxt(f"intrinsics.txt", intrinsics)
+np.savetxt("intrinsics.txt", intrinsics)
